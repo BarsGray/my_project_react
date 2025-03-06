868c3a9c54278f9544351cec74a3aac7ae0dd81a
Daniil <m2140sr@gmail.com>
2025-03-06 05:59:44 +0300

    start project

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
-import ComponentLink from './ComponentLink';
 
-function App() {
-  
+const App = () => {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <ComponentLink />
-      </header>
+      <ul>
+        <li>test 1</li>
+        <li>test 2</li>
+        <li>test 3</li>
+      </ul>
     </div>
   );
 }
